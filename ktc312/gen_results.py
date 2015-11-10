4ea4aed2dd422f0ca553569804758fdcb247b215
ktc312 <ktc312@nyu.edu>
2015-11-10 13:26:02 -0500

    finish gen_results

--- ktc312/gen_results.py
+++ ktc312/gen_results.py
@@ -1,7 +1,13 @@
 __author__ = 'ktc312'
+import numpy as np
+import matplotlib.pyplot as plt
 
 def GenResults(result_output,position,daily_ret):
-    pass
+    with open(result_output,'a')as output:
+        output.write('Number of Positions:  {0}  Mean:  {1}  Std. Dev.:  {2} \n'.format(position,np.mean(daily_ret),np.std(daily_ret)))
 
 def PlotHist(position,daily_ret):
-    pass
+    plt.hist(daily_ret, 100, range = [-1,1])
+    plt.xlim(-1, 1)
+    plt.savefig('histogram_{0}_pos.pdf'.format(str(position).zfill(4)))
+    plt.clf()
