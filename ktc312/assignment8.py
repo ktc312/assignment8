bb84a426d295ce026b0c0fd88c390f44ecdda844
ktc312 <ktc312@nyu.edu>
2015-11-10 11:45:31 -0500

    add main and positions

---
+++ ktc312/assignment8.py
@@ -0,0 +1,9 @@
+from positions import *
+from trial import *
+
+budget = 1000
+odds = 0.51
+result_output = 'results.txt'
+
+def main():
+    position_list = get_position_list()
---
+++ ktc312/positions.py
@@ -0,0 +1,3 @@
+
+def get_position_list():
+    pass
